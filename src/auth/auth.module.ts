5e36b51f850cd062a080b5e99861a81ab0d080ca
Ítalo Lima <contatoitallo@hotmail.com>
2026-06-22 18:14:56 -0300

    refactor(auth): wiring do modulo (provedor USER_GATEWAY, exporta AuthGuard, limpa import duplicado)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,33 +1,32 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './controllers/auth.controller';
 import { AuthUseCase } from './use-cases/auth.use-case';
 import { StackAuthAdapter } from './adapters/stack-auth.adapter';
 import { AuthGuard } from './guards/auth.guard';
-import { ConfigModule } from '@nestjs/config';
-import { UserModule } from '../user/user.module'; 
-import { CompanyModule } from '../company/company.module';
 import { SessionService } from './session.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Session } from './entities/session.entity';
 import { AuthGatewayService } from './auth-gateway.service';
+import { Session } from './entities/session.entity';
+import { USER_GATEWAY } from './ports/user-gateway';
+import { UserGatewayAdapter } from './infrastructure/user/user.gateway.adapter';
+import { UserModule } from '../user/user.module';
+import { CompanyModule } from '../company/company.module';
 import { CompanySettingModule } from '../company-setting/company-setting.module';
 
 @Module({
   imports: [
-    forwardRef(() => UserModule), 
+    forwardRef(() => UserModule),
     forwardRef(() => CompanyModule),
     forwardRef(() => CompanySettingModule),
     TypeOrmModule.forFeature([Session]),
     ConfigModule.forRoot(),
-    CompanySettingModule,
   ],
   controllers: [AuthController],
   providers: [
     AuthGuard,
-    {
-      provide: 'AuthService',
-      useClass: StackAuthAdapter,
-    },
+    { provide: 'AuthService', useClass: StackAuthAdapter },
+    { provide: USER_GATEWAY, useClass: UserGatewayAdapter },
     AuthUseCase,
     SessionService,
     StackAuthAdapter,
@@ -38,8 +37,9 @@ import { CompanySettingModule } from '../company-setting/company-setting.module'
     AuthUseCase,
     SessionService,
     StackAuthAdapter,
-    TypeOrmModule,
     AuthGatewayService,
+    AuthGuard,
+    TypeOrmModule,
   ],
 })
 export class AuthModule {}
